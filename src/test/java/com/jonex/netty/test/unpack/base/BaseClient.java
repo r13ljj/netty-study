c03d42f4c7ae31d9b4ba7f7c32f895055d32e8ca
r13ljj@gmail.com <jl636509313>
2017-08-30 01:47:04 +0800

    unpack demo 1

---
+++ src/test/java/com/jonex/netty/test/unpack/base/BaseClient.java
@@ -0,0 +1,44 @@
+package com.jonex.netty.test.unpack.base;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * Created by Guest on 2017/8/30.
+ */
+public class BaseClient {
+
+    private final static String HOST = System.getProperty("host", "127.0.0.1");
+    private final static int PORT = Integer.parseInt(System.getProperty("post", "9999"));
+    private final static int SIZE = Integer.parseInt(System.getProperty("size", "256"));
+
+    public static void main(String[] args) {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap()
+                    .group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new StringDecoder());
+                            pipeline.addLast(new BaseClientHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.connect(HOST, PORT).sync();
+            future.channel().writeAndFlush("base netty server, i am a common client");
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+}
